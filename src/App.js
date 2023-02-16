952f9dfcc1c0e7d885f9ce8e30b4a58fcd86518e
Rupesh Tiwari <rupeshti@amazon.com>
2023-02-16 17:39:08 -0500

    chore:changing app.js

--- src/App.js
+++ src/App.js
@@ -3,20 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>Welcome to Stable Diffusion AI App</p>
       </header>
     </div>
   );
